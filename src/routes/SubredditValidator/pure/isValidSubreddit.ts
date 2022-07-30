17d294ac0e55621212989c55aa60c229e583b150
asnewman <ashznewman@gmail.com>
2022-07-30 00:22:28 -0700

    Validate subreddit in the backend

---
+++ src/routes/SubredditValidator/pure/isValidSubreddit.ts
@@ -0,0 +1,22 @@
+interface SubredditQueryResult {
+  data: {
+    data: {
+      children: {
+        data: {
+          subreddit_type?: "public" | "private"
+        }
+      }[]
+    }
+  }
+}
+
+async function isValidSubreddit(getSubreddit: () => Promise<SubredditQueryResult>) {
+  try {
+    const res = await getSubreddit();
+    return res.data.data.children[0]?.data.subreddit_type === "public";
+  } catch(e) {
+    return false;
+  }
+}
+
+export { isValidSubreddit, SubredditQueryResult };
---
+++ src/routes/SubredditValidator/subredditValidatorController.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import {isValidSubreddit} from "./pure/isValidSubreddit";
+
+async function subredditValidatorController(req: any, res: any) {
+  const { subreddit } = req.params;
+
+  if (!subreddit) {
+    return res.status(400).send("Must include subreddit in body");
+  }
+
+  const isValid = await isValidSubreddit(() =>
+    axios.get(`https://www.reddit.com/r/${subreddit}/top.json`)
+  );
+
+  if (!isValid) {
+    return res.status(400).send("Subreddit is not valid");
+  }
+
+  return res.status(200).send();
+}
+
+export default subredditValidatorController;
