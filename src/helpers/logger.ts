a9c96cf2f7ec8f201d3c4601b9b5bc1e06834472
asnewman <ashznewman@gmail.com>
2024-04-30 06:25:50 -0700

    remove logzio for now

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 const log = async (level: "info" | "error", message: any) => {
-  try {
-    if (process.env.IS_LOCAL === "false") {
-      await axios.post(
-        `https://listener.logz.io:8071/?token=${process.env.LOGZIO_TOKEN}&type=cyclic-server`,
-        JSON.stringify({
-          message,
-        })
-      );
-    }
-  } catch (e) {
-    console.error(e);
-  }
+  // try {
+  //   if (process.env.IS_LOCAL === "false") {
+  //     await axios.post(
+  //       `https://listener.logz.io:8071/?token=${process.env.LOGZIO_TOKEN}&type=cyclic-server`,
+  //       JSON.stringify({
+  //         message,
+  //       })
+  //     );
+  //   }
+  // } catch (e) {
+  //   console.error(e);
+  // }
 
   if (level === "error") {
     console.error(message);
